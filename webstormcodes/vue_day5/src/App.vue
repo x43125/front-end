fb782de595b58028ad3aa908b08deef6d0d8b7af
x43125 <Za@362113>
2021-06-14 22:04:28 +0800

    day6 webstorm 脚手架 vuex仍不熟悉 要尝试着自己做一个demo

---
+++ webstormcodes/vue_day5/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <br>
+    <router-link to="/">主页</router-link>
+    <router-link to="/user">用户管理</router-link>
+    <router-link to="/student">学生管理</router-link>
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+</style>
---
+++ webstormcodes/vue_day5/src/components/UserAdd.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form action="">
+      姓名：<input type="text" v-model="user.name"><br>
+      年龄：<input type="text" v-model="user.age"><br>
+      薪酬：<input type="text" v-model="user.salary"><br>
+      描述：<input type="text" v-model="user.des"><br>
+      <input type="button" value="保存用户" style="margin-top: 10px" @click="saveUser">
+      <input type="button" value="取消" style="margin-top: 10px" @click="changeRouter">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserAdd",
+  data() {
+    return {
+      user: {},
+    }
+  },
+  methods: {
+    changeRouter() {
+      this.$router.push({name:"User"});
+    },
+    saveUser() {
+      this.$http.post("http://localhost:8081/user/saveOrUpdate", this.user).then(res=>{
+        alert("添加成功");
+        this.$router.push({name:"User"});
+
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
