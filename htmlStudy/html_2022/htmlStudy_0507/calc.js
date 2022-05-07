1f728c353a9e38e089036f2a0680b2f5f0071368
x43125 <wx43125@163.com>
2022-05-07 14:25:30 +0800

    remove anotation

--- htmlStudy/html_2022/htmlStudy_0507/calc.js
+++ htmlStudy/html_2022/htmlStudy_0507/calc.js
@@ -57,57 +57,57 @@ for (var i = 0; i < operator.length; i++) {
 }
 
 // on click of 'equal' button
-// result.addEventListener("click", function () {
+result.addEventListener("click", function () {
 
-//     // this is the string that we will be processing eg. -10+26+33-56*34/23
-//     var inputString = input.innerHTML;
-//     // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-//     var numbers = inputString.split(/\+|\-|\×|\÷/g);
-//     // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-//     // first we replace all the numbers and dot with empty string and then split
-//     var operators = inputString.replace(/[0-9]|\./g, "").split("");
+    // this is the string that we will be processing eg. -10+26+33-56*34/23
+    var inputString = input.innerHTML;
+    // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+    var numbers = inputString.split(/\+|\-|\×|\÷/g);
+    // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+    // first we replace all the numbers and dot with empty string and then split
+    var operators = inputString.replace(/[0-9]|\./g, "").split("");
 
-//     console.log(inputString);
-//     console.log(operators);
-//     console.log(numbers);
-//     console.log("----------------------------");
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("----------------------------");
 
-//     // now we are looping through the array and doing one operation at a time.
-//     // first divide, then multiply, then subtraction and then addition
-//     // as we move we are alterning the original numbers and operators array
-//     // the final element remaining in the array will be the output
-//     var divide = operators.indexOf("÷");
-//     while (divide != -1) {
-//         numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-//         operators.splice(divide, 1);
-//         divide = operators.indexOf("÷");
-//     }
+    // now we are looping through the array and doing one operation at a time.
+    // first divide, then multiply, then subtraction and then addition
+    // as we move we are alterning the original numbers and operators array
+    // the final element remaining in the array will be the output
+    var divide = operators.indexOf("÷");
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
 
-//     var multiply = operators.indexOf("×");
-//     while (multiply != -1) {
-//         numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-//         operators.splice(multiply, 1);
-//         multiply = operators.indexOf("×");
-//     }
+    var multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
 
-//     var subtract = operators.indexOf("-");
-//     while (subtract != -1) {
-//         numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-//         operators.splice(subtract, 1);
-//         subtract = operators.indexOf("-");
-//     }
+    var subtract = operators.indexOf("-");
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
 
-//     var add = operators.indexOf("+");
-//     while (add != -1) {
-//         // using parseFloat is necessary, otherwise it will result in string concatenation :)
-//         numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-//         operators.splice(add, 1);
-//         add = operators.indexOf("+");
-//     }
+    var add = operators.indexOf("+");
+    while (add != -1) {
+        // using parseFloat is necessary, otherwise it will result in string concatenation :)
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
 
-//     input.innerHTML = numbers[0]; // displaying the output
-//     resultDisplayed = true; // turning flag if result is displayed
-// });
+    input.innerHTML = numbers[0]; // displaying the output
+    resultDisplayed = true; // turning flag if result is displayed
+});
 
 // clearing the input on press of clear
 clear.addEventListener("click", function () {
