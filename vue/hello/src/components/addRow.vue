5648ba3f6d3db21b35cfd80b0e9d5b473b9b48e5
x43125 <wx43125@163.com>
2024-03-30 21:34:49 +0800

    feat:update

---
+++ vue/hello/src/components/addRow.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+   <!-- <div v-for="(item,index) in items" :key="index">
+    <div> {{item.key}} </div>
+   </div> -->
+   <button @click="increment">Count is : {{ count }}</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/vue_day7/vue-review-01/src/views/StudyView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+
+    <div>
+      <button @click="addRow">Add Row</button>
+      <table border="1px">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <td>{{ item.name }}</td>
+            <td>{{ item.age }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: 'Alice', age: 30 },
+        { name: 'Bob', age: 25 },
+        { name: 'Charlie', age: 35 }
+      ]
+    };
+  },
+  methods: {
+    addRow() {
+      // 添加一行数据
+      this.items.push({ name: 'Sam', age: 0 });
+    }
+  }
+};
+</script>
